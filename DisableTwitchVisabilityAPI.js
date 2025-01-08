170ce8e521eb0051e225f6c6cfb85d7780b38c12
Jacky Mak <46402241+Iumix@users.noreply.github.com>
2025-01-09 02:25:46 +0900

    Remove redundant version tag

--- DisableTwitchVisabilityAPI.js
+++ DisableTwitchVisabilityAPI.js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name         Disable Twitch Visability API
 // @namespace    http://tampermonkey.net/
-// @version      2024-10-29
 // @description  Disable Twitch Visability API
 // @author       Jacky
 // @version      1.0.1
