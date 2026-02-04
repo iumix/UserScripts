ed56c6e55a8c4885e0fece457df6d0f6e2416b6a
Jacky Mak <46402241+iumix@users.noreply.github.com>
2026-02-04 14:43:16 +0800

    feat: bump version to 1.0.1 for improved artifact removal

--- scripts/RemoveObjectReplacementArtifact.user.js
+++ scripts/RemoveObjectReplacementArtifact.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Object Replacement Artifact
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Removes the Obj artifact on iPhone voice input
 // @author       iumix
 // @match        *://*/*
